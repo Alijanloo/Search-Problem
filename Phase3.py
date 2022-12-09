9a4b5b6c25f589351c2276e89d48fd1b4f4138b8
Ali Javidi Ghasr <71215076+AliJavidiCS@users.noreply.github.com>
2022-12-09 12:18:46 +0330

    Update Phase3.py

--- Phase3.py
+++ Phase3.py
@@ -29,3 +29,31 @@ def BestFS(root_node):
                 pq.put((h, (steps.copy(), n)))
                 visited.append(n)
                 steps.pop(len(steps)-1)
+                
+                
+def A_star(root_node): # uses heuristics to find the answer
+    pq = PriorityQueue()
+    pq.put((0, ([], root_node)))
+    visited = [root_node]
+
+    def is_visited(node):
+        for n in visited:
+            if node.matrix == n.matrix:
+                return True
+        return False
+
+    while not pq.empty():
+        steps, node = pq.get()[1]
+
+        if goal_test(node):
+            return steps, node.cost, node.depth
+
+        next_nodes = successor_func(node)
+        
+        for dir, n in next_nodes.items():
+            if not is_visited(n):
+                steps.append(dir)
+                h = heuristic(n) + n.cost
+                pq.put((h, (steps.copy(), n)))
+                visited.append(n)
+                steps.pop(len(steps)-1)
